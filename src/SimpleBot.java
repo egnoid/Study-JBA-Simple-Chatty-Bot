3b45f9ec063d64409c3657f09028925330a0563d
Nikolai Egorov <egnoid@gmail.com>
2022-05-23 15:57:05 +0300

    first commit

---
+++ src/SimpleBot.java
@@ -0,0 +1,13 @@
+//package bot;
+
+public class SimpleBot {
+    public static void main(String[] args) {
+        // write your code here
+        final String BOTNAME = "Plaksa";
+        final int BOTDOB = 1999;
+        
+        System.out.println("Hello! My name is " + BOTNAME + " .");
+        System.out.println("I was created in " + BOTDOB + " .");
+       
+    }
+}

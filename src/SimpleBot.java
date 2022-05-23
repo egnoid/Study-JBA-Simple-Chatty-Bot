1443deb037cf752cbbb700cc57a483e295d9be9f
Nikolai Egorov <egnoid@gmail.com>
2022-05-23 17:05:24 +0300

    Second commit. Just rollin to GIT

--- src/SimpleBot.java
+++ src/SimpleBot.java
@@ -1,4 +1,5 @@
 //package bot;
+import java.util.Scanner;
 
 public class SimpleBot {
     public static void main(String[] args) {
@@ -8,6 +9,14 @@ public class SimpleBot {
         
         System.out.println("Hello! My name is " + BOTNAME + " .");
         System.out.println("I was created in " + BOTDOB + " .");
+		
+		System.out.println("Please, remind me your name.");
+
+		Scanner scanner = new Scanner(System.in);
+		String yorName = scanner.next();
+		
+		System.out.println("What a great name you have, " + yorName + "!");
+		
        
     }
 }

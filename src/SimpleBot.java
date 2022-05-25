181f4ea7ea734df3e8a617ccab7d22095ae825aa
Nikolai Egorov <egnoid@gmail.com>
2022-05-25 13:11:56 +0300

    4 stage of chatbot project

--- src/SimpleBot.java
+++ src/SimpleBot.java
@@ -27,6 +27,32 @@ public class SimpleBot {
 		int yourAge = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
 		System.out.println("Your age is " + yourAge + "; that's a good time to start programming!");
 		
+		System.out.println("Now I will prove to you that I can count to any number you want.");
+		
+		int inputNum = scanner.nextInt();
+		
+		for (int i = 0; i <= inputNum; i++) {
+			System.out.println(i + "!");
+		}
+		
+		System.out.println("Let's test your programming knowledge.");
+		System.out.println("Why do we use methods?");
+		System.out.println("1. To repeat a statement multiple times.");
+		System.out.println("2. To decompose a program into several small subroutines.");
+		System.out.println("3. To determine the execution time of a program.");
+		System.out.println("4. To interrupt the execution of a program.");
+		
+		int answ = 0;
+		while (answ != 2) {
+			answ = scanner.nextInt();
+			if (answ == 2) {
+				break; 
+			} else {
+				System.out.println("Please, try again.");
+			}
+		}
+		
+		System.out.println("Completed, have a nice day!");
        
     }
 }
